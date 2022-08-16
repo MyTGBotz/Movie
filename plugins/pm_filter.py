49e6698fb60a4dc5fda76434c66e4e5cb94717d0
MyTGBotz <111167531+MyTGBotz@users.noreply.github.com>
2022-08-16 23:13:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -76,7 +76,7 @@ async def next_page(bot, query):
                     text=f"{file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"{[@PenMovieHD]}",
                     callback_data=f'files_#{file.file_id}',
                 ),
             ]
@@ -666,7 +666,7 @@ async def auto_filter(client, msg, spoll=False):
                     callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"{get_size(file.file_size)}",
+                    text=f"{[@PenMovieHD]}",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
             ]

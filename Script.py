306e8cc100892d8d4d26a34fdccebccea64bfb0f
MyTGBotz <111167531+MyTGBotz@users.noreply.github.com>
2022-08-16 17:53:07 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -106,4 +106,4 @@ Added By - {}</b>
 ID - <code>{}</code>
 Name - {}</b>
 """
-M_NT_FND = """<b>This Movie Is Not Found \n\nRequest To Admin</b>
+M_NT_FND = """<b>This Movie Is Not Found \n\nRequest To Admin</b>"""
